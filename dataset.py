168e01f5ca230684c92ab0dea263feb9ddfd2dee
Aishwary Rai <121277048+aishwaryrai@users.noreply.github.com>
2026-04-22 01:25:54 +0530

    Refactor dataset loading and processing
    Updated dataset loading process to use kagglehub for dataset retrieval. Added checks for directory contents and reduced dataset size.

--- dataset.py
+++ dataset.py
@@ -1,15 +1,46 @@
 #Install Libraries
 !pip install transformers pandas scikit-learn rouge-score nltk
 
+!pip install kagglehub
+
+import kagglehub
+
+path = kagglehub.dataset_download("gowrishankarp/newspaper-text-summarization-cnn-dailymail")
+print("Dataset path:", path)
+
+#Check Folder
+import os
+
+for root, dirs, files in os.walk(path):
+    print(root)
+    print(files)
+    print("-"*40)
+
+dataset_path = path + "/cnn_dailymail"
+
 # Load Dataset
 import pandas as pd
-
-dataset_path = "/root/.cache/kagglehub/datasets/gowrishankarp/newspaper-text-summarization-cnn-dailymail/versions/2/cnn_dailymail"
-
 train_df = pd.read_csv(dataset_path + "/train.csv")
 test_df = pd.read_csv(dataset_path + "/test.csv")
 
 print(train_df.head())
+
+import os
+
+dataset_path = "/root/.cache/kagglehub/datasets/gowrishankarp/newspaper-text-summarization-cnn-dailymail/versions/2"
+
+print(os.listdir(dataset_path))
+
+import os
+
+dataset_path = "/root/.cache/kagglehub/datasets/gowrishankarp/newspaper-text-summarization-cnn-dailymail/versions/2"
+
+for root, dirs, files in os.walk(dataset_path):
+    print("FOLDER:", root)
+    print("FILES:", files)
+    print("-"*50)
+
+
 
 
 #Reduce Dataset Size 
